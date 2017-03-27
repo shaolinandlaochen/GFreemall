24136c36f526a08e33845b8f9f49cfa400657706
hanshaolin <707144471@qq.com>
2017-03-27 11:32:50 +0800

    Initial Commit

---
+++ GFreemall/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GFreemall
+//
+//  Created by 韩少林 on 2017/3/27.
+//  Copyright © 2017年 MrHan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ GFreemall/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GFreemall
+//
+//  Created by 韩少林 on 2017/3/27.
+//  Copyright © 2017年 MrHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
