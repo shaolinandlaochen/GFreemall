2ed154277075f037ab25fa4d79a4283ec5548ffb
hanshaolin <707144471@qq.com>
2017-03-30 10:30:43 +0800

    .

---
+++ GFreemall/BrandCell.h
@@ -0,0 +1,14 @@
+//
+//  BrandCell.h
+//  GFreemall
+//
+//  Created by 韩少林 on 2017/3/30.
+//  Copyright © 2017年 MrHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrandCell : UICollectionViewCell
+@property(nonatomic,strong)UIImageView *image;
+@property(nonatomic,strong)UILabel *name;
+@end
---
+++ GFreemall/ListCell.m
@@ -0,0 +1,40 @@
+//
+//  ListCell.m
+//  GFreemall
+//
+//  Created by 韩少林 on 2017/3/29.
+//  Copyright © 2017年 MrHan. All rights reserved.
+//
+
+#import "ListCell.h"
+
+@implementation ListCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+-(instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    if ([super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        autoSize
+        _lbl=[[UILabel alloc]init];
+        _lbl.textColor=[TheParentClass colorWithHexString:@"#de0024"];
+        _lbl.font=[UIFont systemFontOfSize:28*autoSizeScaleY];
+        _lbl.textAlignment=NSTextAlignmentCenter;
+        [self.contentView addSubview:_lbl];
+        _lbl.sd_layout.leftSpaceToView(self.contentView, 0).topSpaceToView(self.contentView, 0).rightSpaceToView(self.contentView, 0).bottomSpaceToView(self.contentView, 0);
+        
+        UILabel *line=[[UILabel alloc]init];
+        line.backgroundColor=[TheParentClass colorWithHexString:@"d7d7d7"];
+        [self.contentView addSubview:line];
+        line.sd_layout.leftSpaceToView(self.contentView, 0).rightSpaceToView(self.contentView, 0).bottomSpaceToView(self.contentView, 0).heightIs(1);
+    }
+    return self;
+}
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
