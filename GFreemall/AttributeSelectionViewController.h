dadab6492f21593bd4a307e32dde4f4329b55c01
hanshaolin <707144471@qq.com>
2017-04-02 10:28:55 +0800

    ..

---
+++ GFreemall/AttributeSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  AttributeSelectionViewController.h
+//  GFreemall
+//
+//  Created by 韩少林 on 2017/4/2.
+//  Copyright © 2017年 MrHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributeSelectionViewController : UIViewController
+
+@end
